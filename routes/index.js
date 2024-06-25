0dab7cf3de092c6b1d7ed128d0a86cfd34c913fd
rabiska <rimal.rabiska@gmail.com>
2024-06-25 13:48:10 +0545

    [FE]:: contact

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const fs = require("fs");
 
 router.get("/", (req, res) => {
-  res.render("/pages/index.ejs");
+  res.render("pages/index.ejs");
 });
 
 module.exports = router;
